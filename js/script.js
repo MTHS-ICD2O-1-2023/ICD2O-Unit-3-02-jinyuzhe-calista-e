ad6c89ff06da7f4108290b6205dfa4f3dd8aa52c
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-03 18:49:49 +0000

    done1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a pyramid.
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const lengthOfPyramid = parseInt(document.getElementById('length-of-pyramid').value)
+  const widthOfPyramid = parseInt(document.getElementById('width-of-pyramid').value)
+  const heightOfPyramid = parseInt(document.getElementById('height-of-pyramid').value)
+
+
+  // process
+  const volumeOfPyramid = (lengthOfPyramid * widthOfPyramid * heightOfPyramid) / 3
+
+  // output
+  document.getElementById('area').innerHTML = 'Volume is: ' + volumeOfPyramid + ' mm³'
+}
